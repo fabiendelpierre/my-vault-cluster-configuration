709a3361059ba6a13cce3b8c6dbdc6232e9d2078
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-01-11 16:26:46 -0500

    formatting

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "vault_auth_backend" "userpass" {
   type = "userpass"
 
   tune {
-    default_lease_ttl   = "600s"
-    max_lease_ttl       = "3600s"
-    listing_visibility  = "unauth"
+    default_lease_ttl  = "600s"
+    max_lease_ttl      = "3600s"
+    listing_visibility = "unauth"
   }
 }
