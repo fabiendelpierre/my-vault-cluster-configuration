178f0e7e522c2a321fb5af61b330d81eee8ae8bc
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-04-14 16:11:17 -0400

    terraform fmt fix

--- auth_oidc.tf
+++ auth_oidc.tf
@@ -16,13 +16,13 @@ resource "vault_jwt_auth_backend_role" "vault_ad_groups" {
   role_name      = "oidc"
   token_policies = ["default"]
 
-  user_claim  = "email"
-  role_type   = "oidc"
+  user_claim = "email"
+  role_type  = "oidc"
 
   allowed_redirect_uris = split(",", var.oidc_allowed_redirect_uris)
 
-  groups_claim  = "groups"
-  oidc_scopes   = ["https://graph.microsoft.com/.default", "profile", "email"]
+  groups_claim = "groups"
+  oidc_scopes  = ["https://graph.microsoft.com/.default", "profile", "email"]
 
   verbose_oidc_logging = true
 }
